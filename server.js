e45aedbb18b686b734534706ee1115f625e73e79
maaislam <maaislam@outlook.com>
2024-08-09 07:33:04 +0600

    initial build

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(bodyParser.json());
 
